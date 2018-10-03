4863daa93739c30755643a7050266b572ba8346e
Ramzey G <rgnet1@gmail.com>
2018-10-03 16:48:30 -0700

    removed passwords from repository

--- config.py
+++
@@ -1,7 +0,0 @@
-# YouTube Video: https://www.youtube.com/watch?v=mP_Ln-Z9-XY
-EMAIL_ADDRESS = "rghanaim@intervision.com"
-PASSWORD = "Hellothere1"
-
-TO_EMAIL_ADDRESS = "rghanaim@intervision.com"
-
-MITCH = "mitchell.sharp@intervision.com"
